12c60195f9fb89c0e66ae0bb2b9dcb268617bf92
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-04-24 20:26:02 +0200

    WIP ex 0_5

---
+++ _Exercices/funciones.py
@@ -0,0 +1,11 @@
+import math
+
+def area_cuadrado_imp(lado):
+    return lado ** 2
+
+def area_triangulo_imp(base, altura):
+    return (base * altura) / 2
+
+def area_circulo_imp(radio):
+    return math.pi * radio ** 2
+
